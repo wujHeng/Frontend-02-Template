7de4ac492185d12449d6f893eeadaba2dec93b01
chensong <754431207@qq.com>
2020-08-21 15:10:37 +0800

    添加API

--- src/api/url.js
+++ src/api/url.js
@@ -1,3 +1,7 @@
 export default {
-  loginUrl: '/api/v1/docs/login'
+  loginUrl: '/api/v1/docs/login',
+  globalTypesUrl: "/api/v1/basics/global-types/",
+  globalCodesUrl: "/api/v1/basics/global-codes/",
+  weighCbUrl: "/api/v1/production/weigh-cb/",
+  weighOilUrl: "/api/v1/production/weigh-oil/",
 }
