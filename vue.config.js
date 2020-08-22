b8494939dbff7d714eedb63788798634dcf6d41f
fengqiang <330538422@qq.com>
2020-08-22 12:15:11 +0800

    proxy

--- vue.config.js
+++ vue.config.js
@@ -38,14 +38,14 @@ module.exports = {
     },
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8001/',
+        target: 'http://192.168.137.79:9000/',
         ws: true,
         changeOrigin: true,
-        pathRewrite:{
-          '^/api':''
+        pathRewrite: {
+          '^/api': ''
         }
       }
-    },
+    }
     // before: require('./mock/mock-server.js')
   },
   configureWebpack: {
