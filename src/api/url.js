85a0695ac4ad0a725d35f840f204de22f79775f0
fengqiang <330538422@qq.com>
2020-08-24 09:10:26 +0800

    格式化

--- src/api/url.js
+++ src/api/url.js
@@ -1,16 +1,15 @@
 export default {
   loginUrl: '/api/v1/docs/login',
-  GlobalTypesUrl: "/api/v1/basics/global-types/", //公共代码类别
-  GlobalCodesUrl: "/api/v1/basics/global-codes/", //公共代码
-  WeighCbUrl: "/api/v1/production/weigh-cb/", //炭黑罐、炭黑称量参数
-  WeighOilUrl: "/api/v1/production/weigh-oil/", //油料罐、油料称量参数
-  WeighUrl: "/api/v1/production/weigh-cb/", //炭黑罐、炭黑称量参数
-  EquipUrl: "/api/v1/basics/equips/", //机台
-  MaterialsUrl: "/api/v1/recipe/materials/", //原材料
+  GlobalTypesUrl: '/api/v1/basics/global-types/', // 公共代码类别
+  GlobalCodesUrl: '/api/v1/basics/global-codes/', // 公共代码
+  WeighCbUrl: '/api/v1/production/weigh-cb/', // 炭黑罐、炭黑称量参数
+  WeighOilUrl: '/api/v1/production/weigh-oil/', // 油料罐、油料称量参数
+  WeighUrl: '/api/v1/production/weigh-cb/', // 炭黑罐、炭黑称量参数
+  EquipUrl: '/api/v1/basics/equips/', // 机台
+  MaterialsUrl: '/api/v1/recipe/materials/', // 原材料
   // 胶料配料标准管理接口
   RubberMaterialUrl: '/api/v1/recipe/product-batching/',
 
-  EquipUrl: '/api/v1/basics/equips/',
   // 报表-批次记录
   ClassesListUrl: '/api/v1/basics/classes/',
   reportBatchUrl: '/api/v1/production/production-record/',
