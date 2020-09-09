5fc24164e684f13459d95d72edf65afe56025d98
chensong <754431207@qq.com>
2020-09-09 19:02:25 +0800

    修改日料罐、称量参数页面机台列表接口参数

--- src/api/weigh.js
+++ src/api/weigh.js
@@ -2,38 +2,37 @@ import request from '@/utils/request'
 import API from '@/api/url'
 
 export function weighCb(method, data = {}) {
-    let obj = {
-        url: API.WeighCbUrl,
-        method: method
-    }
-    Object.assign(obj, data)
-    return request(obj)
+  const obj = {
+    url: API.WeighCbUrl,
+    method: method
+  }
+  Object.assign(obj, data)
+  return request(obj)
 }
 
 export function weighOil(method, data = {}) {
-    let obj = {
-        url: API.WeighOilUrl,
-        method: method
-    }
-    Object.assign(obj, data)
-    return request(obj)
+  const obj = {
+    url: API.WeighOilUrl,
+    method: method
+  }
+  Object.assign(obj, data)
+  return request(obj)
 }
-
 
 export function materials(method, data = {}) {
-    let obj = {
-        url: API.MaterialsUrl,
-        method: method
-    }
-    Object.assign(obj, data)
-    return request(obj)
+  const obj = {
+    url: API.MaterialsUrl,
+    method: method
+  }
+  Object.assign(obj, data)
+  return request(obj)
 }
 
-export function equip(method, data = { params: { all: 1 } }) {
-    let obj = {
-        url: API.EquipUrl,
-        method: method
-    }
-    Object.assign(obj, data)
-    return request(obj)
+export function equip(method, data = { params: { all: 1, category_name: '密炼设备' }}) {
+  const obj = {
+    url: API.EquipUrl,
+    method: method
+  }
+  Object.assign(obj, data)
+  return request(obj)
 }
